f63287c3514ade6977621a7a0f8f7b85592387c9
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2015-03-27 15:15:17 +0200

    ACR_COUNT

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,27 +3,10 @@ set -e
 
 echo 'Webitel ACR '$VERSION
 
-if [ "$ACR_COUNT" ]; then
-	COUNTER=10030
-	ACR_COUNT_STR=10030
-	let ACR_COUNT_MAX=$COUNTER+$ACR_COUNT-1
-	while [  $COUNTER -lt $ACR_COUNT_MAX ]; do
-		let COUNTER=COUNTER+1
-		ACR_COUNT_STR=$ACR_COUNT_STR', '$COUNTER
-	done
-	sed -i 's/ACR_COUNT/'$ACR_COUNT_STR'/g' /acr/conf/config.json
-	echo 'Starting on ports '$ACR_COUNT_STR
-else
-	sed -i 's/ACR_COUNT/10030/g' /acr/conf/config.json
-	echo 'Starting on the port 10030'
-fi
-
 if [ "$LOGLEVEL" ]; then
 	sed -i 's/LOGLEVEL/'$LOGLEVEL'/g' /acr/conf/config.json
-	echo 'Loglevel set to '$LOGLEVEL
 else
 	sed -i 's/LOGLEVEL/warn/g' /acr/conf/config.json
-	echo 'Loglevel set to warn'
 fi
 
 if [ "$LOGSTASH_ENABLE" ]; then
@@ -34,12 +17,10 @@ fi
 
 if [ "$LOGSTASH_HOST" ]; then
 	sed -i 's/LOGSTASH_HOST/'$LOGSTASH_HOST'/g' /acr/conf/config.json
-	echo 'LOGSTASH_HOST set to '$LOGSTASH_HOST
 fi
 
 if [ "$MONGODB_HOST" ]; then
 	sed -i 's/MONGODB_HOST/'$MONGODB_HOST'/g' /acr/conf/config.json
-	echo 'MONGODB_HOST set to '$MONGODB_HOST
 fi
 
 exec node app.js
