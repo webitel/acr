ca46906ff49da80a091dd176e8ab34b946cbb23e
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2015-03-27 14:52:26 +0200

    add ACR_COUNT_STR

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,6 @@ set -e
 
 echo 'Webitel ACR '$VERSION
 
-if [ "$MONGODB_HOST" ]; then
-	sed -i 's/MONGODB_HOST/'$MONGODB_HOST'/g' /acr/conf/config.json
-fi
-
 if [ "$ACR_COUNT" ]; then
 	COUNTER=10030
 	ACR_COUNT_STR=10030
@@ -15,6 +11,7 @@ if [ "$ACR_COUNT" ]; then
 		let COUNTER=COUNTER+1
 		ACR_COUNT_STR=$ACR_COUNT_STR', '$COUNTER
 	done
+	echo 'Starting on ports '$ACR_COUNT_STR
 	sed -i 's/ACR_COUNT/'$ACR_COUNT_STR'/g' /acr/conf/config.json
 else
 	sed -i 's/ACR_COUNT/10030/g' /acr/conf/config.json
@@ -34,6 +31,10 @@ fi
 
 if [ "$LOGSTASH_HOST" ]; then
 	sed -i 's/LOGSTASH_HOST/'$LOGSTASH_HOST'/g' /acr/conf/config.json
+fi
+
+if [ "$MONGODB_HOST" ]; then
+	sed -i 's/MONGODB_HOST/'$MONGODB_HOST'/g' /acr/conf/config.json
 fi
 
 exec node app.js
