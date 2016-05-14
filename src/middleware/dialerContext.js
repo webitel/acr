aa37e80cae0a0f242f333439790d091f7083129b
Navrotskyj <navrotskyj@gmail.com>
2016-05-14 18:12:33 +0300

    test dialer answer

--- src/middleware/dialerContext.js
+++ src/middleware/dialerContext.js
@@ -26,23 +26,29 @@ module.exports = function (conn, destinationNumber, globalVariable, notExistsDir
             return
         }
 
-        let callflow = res._cf;
-        var _router = new CallRouter(conn, {
-            "globalVar": globalVariable,
-            "desNumber": destinationNumber,
-            "chnNumber": destinationNumber,
-            "timeOffset": null,
-            "versionSchema": 2,
-            "domain": domainName
-        });
+        conn.subscribe('CHANNEL_ANSWER');
+        // TODO Channel not answer
+//originate {origination_uuid=b55f4a2f-7953-4964-b536-79203314a2e3,dlr_queue=572a170e576151df0d6b164a,origination_caller_id_number=1234567,origination_caller_id_name=Igor2,gatewayPositionMap=0>0}sofia/gateway/test/380730367300 &socket(10.10.10.25:10030 async full)
+        conn.on('esl::event::CHANNEL_ANSWER::*', (res) => {
+            log.trace('onAnswer');
+            let callflow = res._cf;
+            var _router = new CallRouter(conn, {
+                "globalVar": globalVariable,
+                "desNumber": destinationNumber,
+                "chnNumber": destinationNumber,
+                "timeOffset": null,
+                "versionSchema": 2,
+                "domain": domainName
+            });
 
-        try {
-            log.trace('Exec: %s', destinationNumber);
-            _router.run(callflow);
-        } catch (e) {
-            log.error(e.message);
-            //TODO узнать что ответить на ошибку
-            conn.execute('hangup', DEFAULT_HANGUP_CAUSE);
-        };
+            try {
+                log.trace('Exec: %s', destinationNumber);
+                _router.run(callflow);
+            } catch (e) {
+                log.error(e.message);
+                //TODO узнать что ответить на ошибку
+                conn.execute('hangup', DEFAULT_HANGUP_CAUSE);
+            };
+        });
     });
 };
