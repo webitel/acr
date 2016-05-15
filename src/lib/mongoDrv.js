4c2cb2c04dd90cb9fb72ef25c1ac2a1102d94479
Navrotskyj <navrotskyj@gmail.com>
2016-05-15 14:24:09 +0300

    bug fix

--- src/lib/mongoDrv.js
+++ src/lib/mongoDrv.js
@@ -2,48 +2,62 @@
  * Created by i.navrotskyj on 26.01.2015.
  */
 
-var MongoDb = require("mongodb")
-    , MongoClient = MongoDb.MongoClient
-    , format = require('util').format
-    , config = require('../conf')
-    , log = require('../lib/log')(module);
-
-var mongoClient = new MongoClient();
-
-
-var drv = function (option) {
-
-};
-
-drv._initDB = function (db) {
-    this.db = db;
-    return this.db;
-};
+var MongoDb = require("mongodb"),
+    MongoClient = MongoDb.MongoClient,
+    format = require('util').format,
+    config = require('../conf'),
+    log = require('../lib/log')(module)
+    ;
 
 
-mongoClient.connect(config.get('mongodb:uri') ,function(err, db) {
-    if (err) {
-        log.error('Connect db error: %s', err.message);
-        throw err;
-    };
-    drv._initDB(db);
-
-    log.info('Connected db %s ', config.get('mongodb:uri'));
-    db.on('close', function () {
-        log.error('close mongo');
-    });
-
-    db.on('error', function (err) {
-        log.error(err);
-    });
-});
-
-drv.getCollection = function (name) {
-    try {
-        return this.db.collection(name)
-    } catch (e) {
-        log.error(e.message);
+class Drv {
+    _initDB (db) {
+        this.db = db;
+        return this.db;
     }
-};
+
+    getCollection (name) {
+        try {
+            return this.db.collection(name)
+        } catch (e) {
+            log.error(`mongodb error: ${e.message}`);
+        }
+    }
+}
+
+let drv = new Drv(),
+    timerId = null;
+
+function connect () {
+    if (timerId)
+        clearTimeout(timerId);
+
+    let mongodbClient = new MongoClient(),
+        option = {
+            server: {
+                auto_reconnect: true
+            }
+        }
+    ;
+
+    mongodbClient.connect(config.get('mongodb:uri'), option, function(err, db) {
+        if (err) {
+            log.error('Connect db error: %s', err.message);
+            return timerId = setTimeout(connect, 1000);
+        };
+        drv._initDB(db);
+
+        log.info('Connected db %s ', config.get('mongodb:uri'));
+        db.on('close', function () {
+            log.error('close mongo');
+        });
+
+        db.on('error', function (err) {
+            log.error(err);
+        });
+    });
+}
+
+connect();
 
 module.exports = drv;
