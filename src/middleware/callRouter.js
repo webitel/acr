6b6bd9d92786ece0987391f4411334508e1fe9bb
Navrotskyj <navrotskyj@gmail.com>
2016-06-27 13:11:27 +0300

    bug fix

--- src/middleware/callRouter.js
+++ src/middleware/callRouter.js
@@ -2521,3 +2521,10 @@ CallRouter.prototype.__avmd = function (app, cb) {
 
     return cb && cb();
 };
+
+function reverseString(s) {
+    let o = '';
+    for (let i = s.length - 1; i >= 0; i--)
+        o += s[i];
+    return o;
+}
