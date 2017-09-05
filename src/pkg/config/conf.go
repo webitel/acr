c9df9e9e8fb8da63cf236eaa9f27241b84e61224
Navrotskyj <navrotskyj@gmail.com>
2017-09-05 12:41:50 +0300

    fix flag config

--- src/pkg/config/conf.go
+++ src/pkg/config/conf.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	. "github.com/Nomon/gonfig"
 	"github.com/webitel/acr/src/pkg/logger"
 	"os"
@@ -9,22 +10,15 @@ import (
 var Conf *Gonfig
 
 func init() {
-	filePath := "./conf/config.json"
-
-	for i, s := range os.Args {
-		if s == "-c" {
-			if len(os.Args) > i+1 {
-				filePath = os.Args[i+1]
-			}
-		}
-	}
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		logger.Error("No found config file: " + filePath)
+	filePath := flag.String("c", "./conf/config.json", "Config file path")
+	flag.Parse()
+	if _, err := os.Stat(*filePath); os.IsNotExist(err) {
+		logger.Error("No found config file: " + *filePath)
 		os.Exit(1)
 	}
 
 	Conf = NewConfig(nil)
 	Conf.Use("argv", NewEnvConfig(""))
 	Conf.Use("env", NewEnvConfig(""))
-	Conf.Use("local", NewJsonConfig(filePath))
+	Conf.Use("local", NewJsonConfig(*filePath))
 }
