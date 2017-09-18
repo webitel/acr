503732669a768caa520dacccaf41e4d428905c2d
Navrotskyj <navrotskyj@gmail.com>
2017-09-18 14:45:49 +0300

    add debug key

--- src/acr.go
+++ src/acr.go
@@ -6,19 +6,25 @@ package main
 
 import (
 	"github.com/webitel/acr/src/pkg/acr"
+	"github.com/webitel/acr/src/pkg/config"
 	"github.com/webitel/acr/src/pkg/logger"
 	"net/http"
 	_ "net/http/pprof"
 )
 
 func main() {
+	if config.Conf.Get("dev") == "true" {
+		setDebug()
+	}
+	acr.New()
+}
+
+func setDebug() {
 	//debug.SetGCPercent(-1)
 
 	go func() {
 		logger.Info("Start debug server on :8088")
 		logger.Error("Debug: ", http.ListenAndServe(":8088", nil))
 	}()
-
-	acr.New()
 
 }
