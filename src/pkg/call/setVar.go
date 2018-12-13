44e3f01037d2152587a8c5241febffab2271326a
navrotskyj <navrotskyj@gmail.com>
2018-12-13 17:11:38 +0200

    fixed setVar

--- src/pkg/call/setVar.go
+++ src/pkg/call/setVar.go
@@ -114,8 +114,9 @@ func validateVariable(vars []byte, quote bool) string {
 
 	if l == 2 {
 		res[1] = bytes.TrimSpace(res[1])
-
-		if quote {
+		if len(res[1]) == 0 {
+			res[1] = []byte("''")
+		} else if quote {
 			if !bytes.HasPrefix(res[1], []byte("'")) {
 				res[1] = append([]byte("'"), res[1]...)
 			}
