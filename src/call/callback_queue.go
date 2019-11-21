948def286985f3b160f9a476604f780241984797
navrotskyj <navrotskyj@gmail.com>
2019-11-21 11:37:48 +0200

    [CallbackQueue] more logs

--- src/call/callback_queue.go
+++ src/call/callback_queue.go
@@ -12,7 +12,6 @@ func CallbackQueue(c *Call, args interface{}) error {
 	var props map[string]interface{}
 	var ok bool
 	var number, setVar, comment string
-	var err error
 	var id = 0
 
 	if props, ok = args.(map[string]interface{}); ok {
@@ -28,16 +27,24 @@ func CallbackQueue(c *Call, args interface{}) error {
 		if result.Err != nil {
 			c.LogError("callback", props, result.Err.Error())
 			if setVar != "" {
-				return SetVar(c, setVar+"='"+err.Error()+"'")
+				return SetVar(c, setVar+"='"+result.Err.Error()+"'")
 			}
 			return nil
 		} else {
 			c.LogDebug("callback", map[string]interface{}{"Id": result.Data}, "successful")
 			comment = getStringValueFromMap("comment", props, "")
 			if comment != "" {
-				result = <-c.router.app.Store.CallbackQueue().CreateMemberComment(result.Data.(int64), c.Domain(), "ACR", c.ParseString(comment))
+				memberId := result.Data.(int64)
+				result = <-c.router.app.Store.CallbackQueue().CreateMemberComment(memberId, c.Domain(), "ACR", c.ParseString(comment))
 				if result.Err != nil {
 					c.LogError("callback", args, result.Err.Error())
+				}
+				var i int
+				i, ok = result.Data.(int)
+				if i == 0 {
+					c.LogError("callback_comment", args, "no insert callback comment")
+				} else {
+					c.LogDebug("callback_comment", args, fmt.Sprintf("store new comment %d for member %d", i, memberId))
 				}
 			}
 			if setVar != "" {
